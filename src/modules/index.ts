58ac7bea4fa60a2e6c2952cad3834e5100fdb783
minsuKang <minsu910725@gmail.com>
2022-05-03 09:44:32 +0900

    counter 리덕스

---
+++ src/modules/index.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import counter from './couter';
+
+const rootReducer = combineReducers({
+  counter,
+});
+
+export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
